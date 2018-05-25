3e7a4a3883641581e506cd1d6a646675823c61f2
e-barr <ebarrero04@gmail.com>
2018-05-25 20:46:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function addElementToBeginningOfArray (array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  return array.shift(element)
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray (array, element) {
