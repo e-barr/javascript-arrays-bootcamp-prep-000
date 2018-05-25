ac07d9ed1d46199d007750e6002e50d3b9f6f2b0
e-barr <ebarrero04@gmail.com>
2018-05-25 20:57:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,8 @@ function destructivelyAddElementToEndOfArray (array, element) {
 function accessElementInArray (array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.unshift
+  return array
+}
